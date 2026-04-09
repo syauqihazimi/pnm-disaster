cfe4ec5ee82c8b527c0bc796251282e0cb147813
syauqihazimi <57092537+syauqihazimi@users.noreply.github.com>
2026-04-09 10:36:30 +0700

    Update vite.config.js
    ubah config untuk deploy vercell

--- pnm-disaster/vite.config.js
+++ pnm-disaster/vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Saat dev lokal, proxy /api ke Vercel dev server
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
 });
